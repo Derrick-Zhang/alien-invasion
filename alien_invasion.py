f7f20b8824a04fdbb51849bc068e29ff6119de0c
Hao <haozhang18@fudan.edu.cn>
2020-09-02 18:59:58 +0800

    make the most recently drawn screen visible

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,9 +13,9 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((190,180))
         self.settings.screen_width = self.screen.get_rect().width
-        self.setting.screen_height = self.screen.get_rect().height
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -24,7 +24,6 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -57,6 +56,8 @@ class AlienInvasion:
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
