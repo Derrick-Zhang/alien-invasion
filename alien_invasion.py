898f5566370ca4d63e8df56d7e00e762fa5563ac
Hao <haozhang18@fudan.edu.cn>
2020-09-09 16:24:06 +0800

    display the level

--- alien_invasion.py
+++ alien_invasion.py
@@ -116,6 +116,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -181,6 +182,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """
